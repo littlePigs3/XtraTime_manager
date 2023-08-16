ab63d9dc190cb07d451200f9dfda489b76c7de35
joaofelipe016 <61212164+joaofelipe016@users.noreply.github.com>
2023-08-15 22:21:54 -0300

    inicio

---
+++ src/main/java/com/project/xtratime_manager/XtratimeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.project.xtratime_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtratimeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtratimeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/xtratime_manager/XtratimeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.xtratime_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XtratimeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
