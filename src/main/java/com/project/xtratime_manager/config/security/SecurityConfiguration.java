10c81b58696cb1cda263369bf04d24b59ea161da
joaofelipe016 <61212164+joaofelipe016@users.noreply.github.com>
2023-08-27 09:59:47 -0300

    Update SecurityConfiguration.java

--- src/main/java/com/project/xtratime_manager/config/security/SecurityConfiguration.java
+++ src/main/java/com/project/xtratime_manager/config/security/SecurityConfiguration.java
@@ -2,8 +2,10 @@ package com.project.xtratime_manager.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +16,13 @@ public class SecurityConfiguration  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/usuario").hasRole("AMIN")
+                        .requestMatchers(HttpMethod.POST, "/usuario").hasRole("AMINSETOR")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
-
 }
